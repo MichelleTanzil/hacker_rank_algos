c7ca889e03fd67b94323cd26f2865f2187548847
Michelle Tanzil <mtanzil01@gmail.com>
2020-03-06 08:55:27 -0800

    completed birthday chocolate algo

---
+++ birthday_chocolate/birthday_chocolate.js
@@ -0,0 +1,13 @@
+function birthday(s, d, m) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < m; j++) {
+      sum += s[i + j];
+    }
+    if (sum === d) {
+      count++;
+    }
+  }
+  return count;
+}
